38f149a3f1f950752ec385522852425ce6dc1963
Cristian <cristian.turcios@pinger.com>
2023-02-01 10:13:28 -0600

    added validation to display conditionally users option in navbar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -16,7 +16,8 @@ export const ROUTES: any[] = [{
   ],
   icon: 'ni ni-favourite-28',
   title: 'Adulto Mayor',
-  isCollapsed: true
+  isCollapsed: true,
+  requiredAdmin: false,
 }, {
   submenu: [
     { path: '/discapacitados', title: 'Registros', icon: 'ni-bullet-list-67 text-red', class: '', isCollapsed: true },
@@ -24,16 +25,18 @@ export const ROUTES: any[] = [{
   ],
   title: 'Personas con Discapacidad',
   icon: 'ni ni-world',
-  isCollapsed: true
+  isCollapsed: true,
+  requiredAdmin: false,
 },
-  {
-    submenu: [
-      { path: '/users', title: 'Registros', icon: 'ni-bullet-list-67 text-red', class: '', isCollapsed: true },
-    ],
-    title: 'Usuarios',
-    icon: 'ni ni-circle-08 text-pink',
-    isCollapsed: true
-  }
+{
+  submenu: [
+    { path: '/users', title: 'Registros', icon: 'ni-bullet-list-67 text-red', class: '', isCollapsed: true },
+  ],
+  title: 'Usuarios',
+  icon: 'ni ni-circle-08 text-pink',
+  isCollapsed: true,
+  requiredAdmin: true,
+}
 ];
 
 @Component({
@@ -51,7 +54,15 @@ export class SidebarComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.menuItems = ROUTES.filter(menuItem => menuItem);
+    const isAdmin = this.authService.userValue.isAdmin;
+    
+    this.menuItems = ROUTES.filter((menuItem) => {
+      if (menuItem.requiredAdmin && isAdmin) {
+        return menuItem
+      } else if (!menuItem.requiredAdmin) {
+        return menuItem
+      }
+    });
 
     this.router.events.subscribe((event) => {
       this.isCollapsed = true;
